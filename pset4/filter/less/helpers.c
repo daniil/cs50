0fcee29d550ff778e612c088f269d0ff8cd40248
Daniil Molodkov <yanko86@gmail.com>
2020-12-01 15:13:54 +0000

    Add sepia functionality of filter solution

--- pset4/filter/less/helpers.c
+++ pset4/filter/less/helpers.c
@@ -21,6 +21,23 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float original_red = (float) image[i][j].rgbtRed;
+            float original_green = (float) image[i][j].rgbtGreen;
+            float original_blue = (float) image[i][j].rgbtBlue;
+
+            float sepia_red = fmin(.393 * original_red + .769 * original_green + .189 * original_blue, 255.00);
+            float sepia_green = fmin(.349 * original_red + .686 * original_green + .168 * original_blue, 255.00);
+            float sepia_blue = fmin(.272 * original_red + .534 * original_green + .131 * original_blue, 255.00);
+
+            image[i][j].rgbtBlue = round(sepia_blue);
+            image[i][j].rgbtGreen = round(sepia_green);
+            image[i][j].rgbtRed = round(sepia_red);
+        }
+    }
     return;
 }
 
