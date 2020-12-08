3f342289d249fccd658984f2545917f626e5794f
Daniil Molodkov <yanko86@gmail.com>
2020-12-08 15:25:01 +0000

    Add speller solution

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -1,6 +1,10 @@
 // Implements a dictionary's functionality
-
+#include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #include "dictionary.h"
 
@@ -13,42 +17,150 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = 10001;
 
 // Hash table
 node *table[N];
 
+// Dictionary word count, defaulted to 0
+unsigned int dict_words = 0;
+
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    // Iterate over all words that belong to that hash
+    for (node *tmp = table[hash(word)]; tmp != NULL; tmp = tmp->next)
+    {
+        // Compare strings ignoring case and return true if word is found
+        if (strcasecmp(word, tmp->word) == 0)
+        {
+            return true;
+        }
+    }
+
+    // Word is not found in dictionary return false
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO
-    return 0;
+    unsigned int hashval;
+
+    // Iterate over each character in the word
+    for (hashval = 0; *word != '\0'; word++)
+    {
+        // Lower case the character to ensure non-cased string comparison
+        hashval = tolower(*word) + HASH_MULT * hashval;
+    }
+
+    return hashval % N;
 }
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    dict_file = fopen(dictionary, "r");
+    if (dict_file == NULL)
+    {
+        printf("Could not open %s.\n", dictionary);
+        return false;
+    }
+
+    // Generate hash table
+    populate_table();
+
+    return true;
+}
+
+// Generates a dictionary hash table with words
+bool populate_table(void)
+{
+    int index = 0;
+    char word[LENGTH + 1];
+
+    // Iterate over each string of the file
+    while (fscanf(dict_file, "%s", word) != EOF)
+    {
+        // Count words
+        dict_words++;
+
+        // Add new word to dictionary table
+        add_to_table(word);
+    }
+
+    return true;
+}
+
+// Adds a word to the dictionary table
+bool add_to_table(char *word)
+{
+    struct node *tmp;
+    unsigned int hashval;
+
+    tmp = (struct node *) malloc(sizeof(*tmp));
+    if (tmp == NULL)
+    {
+        return false;
+    }
+
+    // Copy word into the node
+    strcpy(tmp->word, word);
+
+    // Calculate the hash of the word
+    hashval = hash(word);
+
+    if (table[hashval] == NULL)
+    {
+        // If there is no node at current hash table value, set next of node to NULL
+        tmp->next = NULL;
+    }
+    else
+    {
+        // Set temp node's next pointer to current head node of the hash table at hash value
+        tmp->next = table[hashval];
+    }
+
+    // Set hash table at hash value to temp node
+    table[hashval] = tmp;
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    // We already calculate the length while loading, we can return it right away
+    return dict_words;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    // Iterate over hash table
+    for (int i = 0; i < N; i++)
+    {
+        // Check if table not NULL
+        if (table[i] == NULL)
+        {
+            continue;
+        }
+
+        // Free hash table nodes going until the last node is NULL
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *tmp = cursor;
+            cursor = cursor->next;
+            free(tmp);
+        }
+
+        // Free the last node as well
+        free(cursor);
+    }
+
+    // Close the dictionary file
+    fclose(dict_file);
+
+    return true;
 }
--- pset5/speller/dictionary.h
+++ pset5/speller/dictionary.h
@@ -9,11 +9,20 @@
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
 
+// Multiplier used for hashing function
+// 31 is an odd prime helping with avoiding collisions
+#define HASH_MULT 31
+
+// Global Variables
+FILE *dict_file;
+
 // Prototypes
 bool check(const char *word);
 unsigned int hash(const char *word);
 bool load(const char *dictionary);
 unsigned int size(void);
 bool unload(void);
+bool populate_table(void);
+bool add_to_table(char *word);
 
 #endif // DICTIONARY_H
