eebf2bcc1fdd127a9661361f0e332848b71833b9
Daniil Molodkov <yanko86@gmail.com>
2020-12-10 12:40:39 +0000

    Add week 6 mario/more solution

---
+++ pset6/mario/more/mario.py
@@ -0,0 +1,27 @@
+from cs50 import get_int
+
+
+def main():
+    # Iterate until user inputs correct value
+    while(True):
+        cols = get_int("Height: ")
+
+        # Input needs to be between 1-8
+        if cols > 0 and cols < 9:
+            break
+
+    # Print the pyramid
+    print_pyramid(cols)
+
+
+# Prints equal size pyramid
+def print_pyramid(cols):
+    for i in range(1, cols + 1):
+        print(" " * (cols - i), end="")
+        print("#" * i, end="")
+        print(" " * 2, end="")
+        print("#" * i)
+
+
+if __name__ == "__main__":
+    main()
